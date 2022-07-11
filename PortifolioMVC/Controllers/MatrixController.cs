ffc5bf52cacd955c441594244233040b4a48a4b9
Inveja <halysso09@gmail.com>
2022-07-11 11:55:04 -0300

    Matrix

---
+++ PortifolioMVC/Controllers/MatrixController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortifolioMVC.Models;
+using System.Diagnostics;
+
+namespace PortifolioMVC.Controllers
+{
+    public class MatrixController : Controller
+    {
+        private readonly ILogger<MatrixController> _logger;
+
+        public MatrixController(ILogger<MatrixController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
